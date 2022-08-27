f3c5a70807cafb874d15de2a79101ee9d0e3180f
hjmv11 <110635467+hjmv11@users.noreply.github.com>
2022-08-27 04:42:53 +0000

    got started on budget

--- budget.py
+++ budget.py
@@ -1,7 +1,42 @@
 class Category:
+  def __init__(self, name):
+    self.name = name
+    ledger = []
 
+  def deposit(amount, descr=""):
+    ledger.append({"amount": amount,"description": descr})
 
+  def get_balance:
+    for(transaction in ledger):
+      balance += transaction["amount"]
+      return balance
 
+  def check_funds(amount):
+    if(amount >= 0):
+      if(amount > self.get_balance):
+        return False
+      else:
+        return True
+    else:
+      if(-(amount) > self.get_balance):
+        return False
+      else: 
+        return True
 
+  def withdraw(amount, descr):
+    if(check_funds(amount)): 
+      ledger.append({"amount": amount, "description": descr})
+      return True
+    else:
+      return False
 
+  def transfer(amount, category):
+    destination_category = Category(category)
+    if(self.check_funds):
+      self.withdraw(amount, "Transfer to " + category)
+      destination_category.deposit(amount, "Transfer from " + category)
+      return True
+    else:
+      return False 
+      
 def create_spend_chart(categories):
