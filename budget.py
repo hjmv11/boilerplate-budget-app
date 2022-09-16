fe47999e250c2835021778c8064ab6f4fb4f2307
hjmv11 <110635467+hjmv11@users.noreply.github.com>
2022-09-16 20:56:18 +0000

    stuff

--- budget.py
+++ budget.py
@@ -1,42 +1,48 @@
 class Category:
+  
   def __init__(self, name):
     self.name = name
-    ledger = []
+    self.ledger = []
+    self.balance = 0.00
 
-  def deposit(amount, descr=""):
-    ledger.append({"amount": amount,"description": descr})
+  def deposit(self, amount, descr=" "):
+    if descr:
+      self.ledger.append({"amount": amount,"description": descr})
+    else:
+      self.ledger.append({"amount": amount,"description": " "})
 
-  def get_balance:
-    for(transaction in ledger):
-      balance += transaction["amount"]
-      return balance
+  def get_balance(self):
+    for transaction in self.ledger:
+      self.balance += transaction["amount"]
+      return self.balance
 
-  def check_funds(amount):
+  def check_funds(self, amount):
     if(amount >= 0):
-      if(amount > self.get_balance):
+      if(amount > self.balance):
         return False
       else:
         return True
     else:
-      if(-(amount) > self.get_balance):
+      if(-(amount) > self.balance):
         return False
       else: 
         return True
 
-  def withdraw(amount, descr):
-    if(check_funds(amount)): 
-      ledger.append({"amount": amount, "description": descr})
+  def withdraw(self, amount, descr=" "):
+    if(self.check_funds(amount)): 
+      self.ledger.append({"amount": amount, "description": descr})
       return True
     else:
       return False
 
-  def transfer(amount, category):
+  def transfer(self, amount, category):
     destination_category = Category(category)
     if(self.check_funds):
-      self.withdraw(amount, "Transfer to " + category)
-      destination_category.deposit(amount, "Transfer from " + category)
+      self.withdraw(amount, "Transfer to " + category.name)
+      destination_category.deposit(amount, "Transfer from " + self.name)
       return True
     else:
       return False 
       
 def create_spend_chart(categories):
+    return "poop"
