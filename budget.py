eaac3c9e163cef7cbf6d771191fa759faf0d90e0
hjmv11 <110635467+hjmv11@users.noreply.github.com>
2022-09-18 19:09:29 +0000

    stuff

--- budget.py
+++ budget.py
@@ -5,44 +5,48 @@ class Category:
     self.ledger = []
     self.balance = 0.00
 
-  def deposit(self, amount, descr=" "):
-    if descr:
-      self.ledger.append({"amount": amount,"description": descr})
-    else:
-      self.ledger.append({"amount": amount,"description": " "})
+  def __str__(self):
+    output = ""
+    adjustment = int(((30 - len(self.name))/2) + len(self.name))
+    output +=  self.name.ljust(adjustment,'*').rjust(30,'*') + '\n'
+
+    for transaction in self.ledger:
+      output += transaction["description"][:23].ljust(23,' ') + "{:.2f}".format(transaction["amount"]).rjust(7,' ')  + '\n'
+
+    output += 'Total: ' + str(self.balance)
+    
+    return output
+
+  def deposit(self, amount, descr=""):
+    self.ledger.append({"amount": amount,"description": descr})
+    self.balance += amount
 
   def get_balance(self):
-    for transaction in self.ledger:
-      self.balance += transaction["amount"]
-      return self.balance
+    return self.balance
 
   def check_funds(self, amount):
-    if(amount >= 0):
-      if(amount > self.balance):
-        return False
-      else:
-        return True
+    if(amount > self.get_balance()):
+      return False
     else:
-      if(-(amount) > self.balance):
-        return False
-      else: 
-        return True
+      return True
 
-  def withdraw(self, amount, descr=" "):
+  def withdraw(self, amount, descr=""):
     if(self.check_funds(amount)): 
-      self.ledger.append({"amount": amount, "description": descr})
+      self.ledger.append({"amount": -amount, "description": descr})
+      self.balance -= amount
       return True
     else:
       return False
 
   def transfer(self, amount, category):
-    destination_category = Category(category)
-    if(self.check_funds):
+    if(self.check_funds(amount)):
       self.withdraw(amount, "Transfer to " + category.name)
-      destination_category.deposit(amount, "Transfer from " + self.name)
+      category.deposit(amount, "Transfer from " + self.name)
       return True
     else:
-      return False 
-      
+      return False
+
+  
+  
 def create_spend_chart(categories):
-    return "poop"
+  return 'poop'
