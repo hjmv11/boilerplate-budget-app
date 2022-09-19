f2520b3e0053c711d3892183eafa9811aca51e44
hjmv11 <110635467+hjmv11@users.noreply.github.com>
2022-09-19 21:48:12 +0000

    added comments

--- budget.py
+++ budget.py
@@ -1,10 +1,12 @@
 class Category:
-  
+
+  #set class object properties when created 
   def __init__(self, name):
     self.name = name
     self.ledger = []
     self.balance = 0.00
 
+  #set result when class object printed
   def __str__(self):
     output = ""
     adjustment = int(((30 - len(self.name))/2) + len(self.name))
@@ -17,19 +19,23 @@ class Category:
     
     return output
 
+  #amount provided added to balance
   def deposit(self, amount, descr=""):
     self.ledger.append({"amount": amount,"description": descr})
     self.balance += amount
 
+  #balance
   def get_balance(self):
     return self.balance
 
+  #check if amount provide is greater than balance
   def check_funds(self, amount):
     if(amount > self.get_balance()):
       return False
     else:
       return True
 
+  #balance subtracted by negative amount provided 
   def withdraw(self, amount, descr=""):
     if(self.check_funds(amount)): 
       self.ledger.append({"amount": -amount, "description": descr})
@@ -38,6 +44,7 @@ class Category:
     else:
       return False
 
+  #withdraw amount provided and deposit to category provided, if funds sufficient funds available
   def transfer(self, amount, category):
     if(self.check_funds(amount)):
       self.withdraw(amount, "Transfer to " + category.name)
@@ -47,17 +54,20 @@ class Category:
       return False
 
   
-  
+
 def create_spend_chart(categories):
+  #create output string
   output = "Percentage spent by category\n"
-  
+
+  #width after the pipe 
   width = (len(categories)*3)+1
-  
+
+  #spending total, total spent by category, category name length variables 
   total = 0
   category_totals = {}
   category_name_length = []
   
-  
+  #loop through categories provided to populated variables 
   for i in range(0,len(categories)):
     category_total = 0 
     for transaction in categories[i].ledger:
@@ -67,13 +77,14 @@ def create_spend_chart(categories):
     category_name_length.append(len(categories[i].name))
     total += round(category_total,2) 
 
-  print(total)
-  print(category_totals)
-
+  #loop through numbers and percentage spent by category
   for i in range(100,-10,-10):
+    #adds number, pipe and space
     output += (str(i) + "| ").rjust(5)
     for j in range(0,len(categories)):
+      #if percentage times 100 is greather or equal to the current number add 'o' for the current category, if not add blanks 
       if int(round((category_totals[j]/total)*100,-1)) >= i:
+        #if the first category no spaces, if not 2 spaces behind the o 
         if j == 0:
           output += "o".rjust(j)
         else:  
@@ -83,16 +94,21 @@ def create_spend_chart(categories):
           output += " ".rjust(j)
         else:  
           output += " ".rjust(3)
+    #add two spaces and end of line character
     output += "  \n"
 
+  #add dashed line
   line = "-"*width
   output += line.rjust(width+4)+"\n"
 
+  #get the longest category name's length 
   category_name_length.sort(reverse = True)
   max_length = category_name_length[0]
-  
+
+  #loop through each letter for each category name provided
   for i in range(0,max_length):
     for j in range(0,len(categories)):
+      #if the current category name is smaller than the longest category name add blanks, if not add the character 
       if i > len(categories[j].name)-1:
         if j == 0:
           output += " ".rjust(6)
@@ -103,6 +119,7 @@ def create_spend_chart(categories):
           output += categories[j].name[i].rjust(6)
         else:
           output += categories[j].name[i].rjust(3)
+    #if the last character of the longest category name exclude end of line character, ifnot include 
     if i == max_length-1:
       output += "  "
     else:
